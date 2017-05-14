41bce9381273dc7d2da74a5fcaf1ee9c1dcd87d4
Benny Neugebauer <bn@bennyn.de>
2017-05-14 15:37:48 +0200

    Deployment via Travis CI (#8)

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (request, response) => {
-  response.send('<p>Hello World!</p>');
+  response.send('<p>Hello from Travis CI!</p>');
 });
 
 const server = app.listen(port, () => {
