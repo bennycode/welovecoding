d3744a34d53a87484cbe1305bf9b2d5713faf455
Benny Neugebauer <bn@bennyn.de>
2017-05-14 14:29:56 +0200

    Fixing port for Heroku deployment

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.get('/', (request, response) => {
   response.send('<p>Hello World!</p>');
 });
 
-const server = app.listen(8080, () => {
+const server = app.listen(port, () => {
   const port = server.address().port;
   console.log(`Server is running. Try http://localhost:${port}/`);
 });
