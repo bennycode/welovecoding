445595e205ca5b97ee5c92428a9b3878d86ff271
Benny Neugebauer <bn@bennyn.de>
2017-05-16 15:57:50 +0200

    Replaced "build.sh" with "build.js", resolves #10

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
 const exec = require('child_process').exec;
+const fs = require('fs-extra');
 const path = require('path');
-// const fs = require('fs-extra');
 
 function executeCommand(command) {
   return new Promise(function (resolve) {
-    console.log(`Running command: ${command}`);
+    console.log(`Running command: "${command}"`);
     const child = exec(command);
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
@@ -15,17 +15,25 @@ function executeCommand(command) {
 }
 
 Promise.resolve()
-  .then(() => {
-    return executeCommand(`cd ${path.join(__dirname, 'backend')} && npm install && npm run build`);
-  })
-  .then(() => {
-    return executeCommand(`cd ${path.join(__dirname, 'frontend')} && npm install && npm install --only=dev && npm run build`);
-  })
-  .then(function () {
-    console.log('Done');
-    process.exit(0);
-  })
-  .catch((function (error) {
-    console.error(error.message, error);
-    process.exit(1);
-  }));
+.then(() => {
+  // build backend
+  return executeCommand(`cd ${path.join(__dirname, 'backend')} && npm install && npm run build`);
+})
+.then(() => {
+  // build frontend
+  return executeCommand(`cd ${path.join(__dirname, 'frontend')} && npm install && npm install --only=dev && npm run build`);
+})
+.then(() => {
+  // copy frontend dist to backend
+  return fs.ensureDir(path.join(__dirname, 'backend/dist')).then(() => {
+    return fs.move(path.join(__dirname, 'frontend/dist'), path.join(__dirname, 'backend/dist/frontend'));
+  });
+})
+.then(function () {
+  console.log('Done');
+  process.exit(0);
+})
+.catch((function (error) {
+  console.error(error.message, error);
+  process.exit(1);
+}));
