aa30210913707dee2fa124eea4a05cf91909a037
TN1ck <tomwanick@gmail.com>
2017-06-08 15:15:01 +0200

    fix: linting, some rules, adds source-map support for the backend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
 import App from './App';
 
-require('../assets/index.scss')
+require('../assets/index.scss');
 
 function renderApp(RootComponent) {
   ReactDOM.render(
