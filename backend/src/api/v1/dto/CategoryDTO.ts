1f5b65386f2f26dd56ff9a279d248aa2a02c9508
Benny Neugebauer <bn@bennyn.de>
2017-06-14 23:22:06 +0200

    chore

--- backend/src/api/v1/dto/CategoryDTO.ts
+++ backend/src/api/v1/dto/CategoryDTO.ts
@@ -1,9 +1,6 @@
 import BaseDTO from './BaseDTO';
 import PlaylistDTO from './PlaylistDTO';
 
-// https://github.com/bennyn/wlc-webapp/blob/wlc-webapp/0.0.36/src/
-// main/java/com/welovecoding/tutorial/api/v1/dto/CategoryDTO.java
-// http://www.welovecoding.com/rest/service/v1/categories
 export default class CategoryDTO extends BaseDTO {
   public color: string;
   public playlists: PlaylistDTO[] = [];
@@ -23,7 +20,7 @@ export default class CategoryDTO extends BaseDTO {
   get numberOfVideos(): number {
     let numberOfVideos: number = 0;
 
-    this.playlists.forEach(function(playlist: PlaylistDTO) {
+    this.playlists.forEach((playlist: PlaylistDTO) => {
       numberOfVideos += playlist.numberOfVideos;
     });
 
