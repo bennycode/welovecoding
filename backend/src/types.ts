9e4c16e3d07d6029a6e796755f8461a7dbeac77a
Benny Neugebauer <bn@bennyn.de>
2017-06-14 23:51:43 +0200

    Sorting

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,26 +1,26 @@
 export interface GoogleOAuthProfile {
-  kind: string;
-  etag: string;
-  gender: string;
-  emails: Array<{value: string; type: string}>;
-  objectType: string;
-  id: string;
-  displayName: string;
-  name: {
-    familyName: string;
-    givenName: string;
-  };
-  url: string;
-  image: {
-    url: string;
-    isDefault: boolean;
-  };
-  isPlusUser: boolean;
-  language: string;
   ageRange: {
     min: number;
   };
   circledByCount: number;
-  verified: boolean;
   cover: any;
+  displayName: string;
+  emails: Array<{value: string; type: string}>;
+  etag: string;
+  gender: string;
+  id: string;
+  isPlusUser: boolean;
+  image: {
+    url: string;
+    isDefault: boolean;
+  };
+  kind: string;
+  language: string;
+  name: {
+    familyName: string;
+    givenName: string;
+  };
+  objectType: string;
+  url: string;
+  verified: boolean;
 }
